634aa92831641d3ce11a4add00eb1822deef4d75
jorgemago <jorgero.nunez@gmail.com>
2023-09-24 23:26:39 -0300

    subida de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    CarruselComponent
   ],
   imports: [
     BrowserModule,
